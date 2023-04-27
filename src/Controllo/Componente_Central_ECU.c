9a2fc04a9cc1842e94493da4e31e77fcb5f71716
Martin Ligabue <martinligabue@gmail.com>
2023-04-27 11:54:52 +0200

    tenatativi scarsi

---
+++ src/Controllo/Componente_Central_ECU.c
@@ -0,0 +1,29 @@
+/*
+Tutti i comandi inviati dalla Central ECU a qualunque componente sono inseriti in un file di log ECU.log e stampati a video tramite la HMI.
+La Central ECU effettua le seguenti azioni. All’avvio del sistema, imposta la velocità attuale a 0, e ignora qualsiasi informazione ricevuta dai sensori, finchè riceve dall’HMI il comando “INIZIO”. Questo indica l’avvio del percorso.
+Dopo aver ricevuto il comando INIZIO, durante la sua esecuzione, la Central ECU riceve continuamente dati dai sensori.
+*/
+#include <stdio.h>
+
+int componenthminterface(char* comando){
+    while(1==1)
+        printf(comando);
+
+    int velocita_attuale = 0;
+
+    bool inizio = false;
+    while (inizio == false){
+        if (comando == "INIZIO"){
+            inizio = true;
+        }
+    }
+    while (inizio == true){
+        if (comando == "STOP"){
+            inizio = false;
+        }
+        else{
+            velocita_attuale = velocita_attuale + 1;
+        }
+    }
+    return 0;
+}
---
+++ src/Interfaccia/componente_Human-machine_interface.c
@@ -0,0 +1,10 @@
+/*
+Questo componente interagisce con la Central ECU, per mostrare a video, come output, quanto comunicato dalla Central ECU, e per inviare input ricevuti dall’utente tramite la tastiera.
+La gestione dell’input ed output avviene tramite due terminali. Un terminale è utilizzato solo per mostrare i valori di output, mentre l’altro terminale è utilizzato solo per raccogliere e inviare input alla Central ECU.
+Come output, il processo stampa a schermo tutte le azioni comandate dalla Central ECU.
+*/
+#include <stdio.h>
+
+int componenthminterface(){
+    
+}
