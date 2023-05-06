65ee005e848f3256970c98587e08194925bcb520
Martin Ligabue <martinligabue@gmail.com>
2023-05-06 14:03:21 +0200

    todos

--- src/Attuatori/steer-by-wire.c
+++ src/Attuatori/steer-by-wire.c
@@ -2,3 +2,7 @@
 Questo componente riceve dalla Central ECU il comando di girare a DESTRA o SINISTRA, e conseguentemente attiva lo sterzo. L’azione di girare dura 4 secondi.
 Ogni secondo, il componente stampa nel file di log steer.log: “NO ACTION”, “STO GIRANDO A DESTRA”, “STO GIRANDO A SINISTRA”, sulla base dell’azione in corso
 */
+#include <stdio.h>
+
+int main() {
+}
